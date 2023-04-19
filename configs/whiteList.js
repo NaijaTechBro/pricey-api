8c0fd5c57e485abb8b3d308f3b06c9006d71bf8c
NaijaTechBro <bakisodiq@gmail.com>
2023-04-19 01:29:59 -0700

    server setup

---
+++ configs/whiteList.js
@@ -0,0 +1,12 @@
+const whiteList = [
+    'http://localhost:3000',
+    'http://www.naijatechbro.com',
+    'http://localhost:7890/',
+    'https://www.swagger.io',
+    'http://127.0.0.1:5173/',
+    'https://www.google.com'
+
+]
+
+
+module.exports = whiteList
---
+++ constants/index.js
@@ -0,0 +1,4 @@
+exports.ROLE_ADMIN = 'ROLE_ADMIN';
+exports.ROLE_USER = 'ROLE_USER';
+exports.ROLE_CLIENT = 'ROLE_CLIENT';
+exports.ROLE_BUSINESS = 'ROLE_BUSINESS';
